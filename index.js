7037b00c619fc2b1e27b5ae89a8d912e4eb45189
XLLU <lucasluu@yahoo.com>
2019-07-07 14:12:46 +0800

    ..

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-const app = new Vue({
-	el:'#app'
-})
+
+// 1 Base: 
+// const app = new Vue({
+// 	el:'#app', 
+// 	// state  数据源
+// 	data () {
+// 		return {
+// 		  count: 0
+// 		}
+// 	},
+// 	// view  视图
+// 	template: `
+// 		<div @click="increment" class="ui button">{{ count }}</div>
+// 	`,
+// 	// actions  事件
+// 	methods: {
+// 		increment () {
+// 		  this.count++
+// 		}
+// 	}
+// })
+
+// 2 Component
+// Vue.component('ui-button', {
+// 	template: '<button @click="increment" class="ui button">{{ counter }}</button>', 
+// 	data() {
+// 		return {
+// 			counter: 0
+// 		}
+// 	},
+// 	methods: {
+// 		increment() {
+// 			this.counter ++
+// 			// this.$emit('increment')
+// 		}
+// 	}
+// })
+// 
+// const app = new Vue({
+// 	el:'#app'
+// })
